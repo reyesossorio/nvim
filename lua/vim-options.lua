130262251a99f210c5e745912b0033a68bbb6f80
Reyossrom99 <reyesossorio@outlook.com>
2025-02-13 17:47:32 +0100

    first commit

---
+++ lua/vim-options.lua
@@ -0,0 +1,12 @@
+
+vim.g.mapleader = " "
+vim.g.background = "light"
+
+vim.wo.number = true
+vim.wo.relativenumber = true
+
+vim.opt.shiftwidth = 4     -- Indentation width = 4 spaces
+vim.opt.smarttab = true    -- Use Tab key for indentation intelligently
+vim.opt.expandtab = true   -- Convert tab presses into spaces
+vim.opt.tabstop = 8        -- Tabs display as 8 spaces to distinguish them from 4-space indents
+vim.opt.softtabstop = 0    -- Prevent mixing of spaces/tabs when backspacing
